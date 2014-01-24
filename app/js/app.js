7574ee0e42e8683454e69d665118762b9111edc3
Rob Brown <rabrown.mail@gmail.com>
2014-01-23 19:58:11 -0500

    Fixed filter and added some more dynamic results.

--- app/js/app.js
+++ app/js/app.js
@@ -2,22 +2,39 @@
 
 
 // Declare app level module which depends on filters, and services
-var app = angular.module('RPSLS', []);
+var app = angular.module('RPSLS', ['RPSLS.filters']);
 
 
 function gameCtrl($scope) {
     $scope.playerChoice = "";
-    $scope.computerChoice = "Lizard";
+    $scope.computerChoice = "";
     $scope.playerScore = 0;
     $scope.computerScore = 0;
+    $scope.ties = 0;
+    $scope.result = "";
+
+    var gameChoices = [0,1,2,3,4];
 
     $scope.playGame = function() {
-        console.log($scope.playerChoice + $scope.computerChoice);
-        $scope.playerScore++;
+        $scope.computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+        var results = (5 + $scope.playerChoice - $scope.computerChoice) % 5;
+        if (results == 1 || results == 3) {
+            $scope.playerScore++;
+            $scope.result = "Winner!";
+        }
+        else if (results == 2 || results == 4) {
+            $scope.computerScore++;
+            $scope.result = "Sorry, you didn't win.";
+        }
+        else {
+            $scope.ties++;
+            $scope.result = "It's a tie!";
+        }
     };
 
     $scope.resetGame = function() {
-        $scope.playerChoice = "";
+        //$scope.playerChoice = "";
+        //$scope.computerChoice = "";
         $('a.close-reveal-modal').trigger('click');
     };
 }
--- app/js/filters.js
+++ app/js/filters.js
@@ -2,9 +2,27 @@
 
 /* Filters */
 
-angular.module('myApp.filters', []).
-  filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    }
-  }]);
+angular.module('RPSLS.filters', []).
+  filter('parseResults', function() {
+    return function(input) {
+      switch(parseInt(input))
+        {
+        case 0:
+          input = "Rock";
+          break;
+        case 1:
+          input = "Paper";
+          break;
+        case 2:
+          input = "Scissors";
+          break;
+        case 3:
+          input = "Spock";
+          break;
+        case 4:
+          input = "Lizard";
+          break;
+        }
+        return input;
+    };
+  });
